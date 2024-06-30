e8175e5722f22c90e0931463e728e6b1404ec2b0
Aman Srivastava <credevator@outlook.com>
2024-06-30 18:59:18 +0000

    Add TODO.

--- alt/commands/acquia/local_sync.py
+++ alt/commands/acquia/local_sync.py
@@ -1,7 +1,7 @@
 import subprocess
 
 import click
-
+from alt.helpers.package import composer
 
 @click.command()
 @click.argument('machinename')
@@ -10,7 +10,15 @@ def local_sync_command(machinename):
     click.echo(f"Syncing local environment with Acquia environment '{machinename}'...")
     try:
         # Example sync command; replace with actual sync instructions
-        subprocess.run(["bash", "sync-environment.sh", machinename], check=True)
+        #subprocess.run(["bash", "sync-environment.sh", machinename], check=True)
+        composer.is_package_installed('drupal/acsf')
+        #TODO: Check if it is ACSF or ACE
+
+        #TODO: Read the alias.
+
+        #TODO: Check if DB is available.
+
+        #TODO: Run drush sql:sync @alias @self -machinename
         click.echo(f"Local environment synced with Acquia environment '{machinename}' successfully.")
     except subprocess.CalledProcessError as e:
         click.echo(f"Failed to sync environment: {e}", err=True)
